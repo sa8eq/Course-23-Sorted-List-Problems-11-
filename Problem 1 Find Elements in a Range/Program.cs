7be45f34c83c6ed06a2ec8d0baa732d9a910db21
sa8eq <sadeq8omar@gmail.com>
2026-06-14 10:21:25 +0300

    init

---
+++ Problem 1 Find Elements in a Range/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1_Find_Elements_in_a_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
